37aac346cfb791e90df3aa47a25c653b33bae132
iznoelc <izzybellanoel@gmail.com>
2025-11-21 14:40:01 -0500

    mostly working puzzle 3 :3

--- src/Level_3/Level_3.java
+++ src/Level_3/Level_3.java
@@ -11,60 +11,143 @@ public class Level_3 extends JPanel {
     private Spaceship gleebusShip = new BrokenSpaceship();
     private Game parent;
 
+    private boolean leverComponentBuilt = false;
+    private boolean buttonPanelComponentBuilt = false;
+    private boolean screenComponentBuilt = false;
+
+    JCheckBox hammer, wrench, pliers, screwdriver;
+
     public Level_3(Game parent){
         this.parent = parent;
         setLayout(new BorderLayout());
 
+        // use a layered pane since we need to layer buttons, checkboxes, etc. over drawings
+        // set up the layered pane
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setPreferredSize(new Dimension(1280,720));
         add(layeredPane, BorderLayout.CENTER);
 
+        // set up the drawing panel to draw the images and add it to the layered pane
         DrawPanel drawingPanel = new DrawPanel();
-        drawingPanel.setBounds(0,0,1280,720);
+        drawingPanel.setBounds(0,-25,1280,720);
         layeredPane.add(drawingPanel, JLayeredPane.DEFAULT_LAYER);
 
-        JPanel buttonPanel = getButtonJPanel(parent);
-        buttonPanel.setBounds(0, 0, 1280, 100); // position at top
+        JPanel checkboxPanel = getCheckBoxPanel(parent);
+        checkboxPanel.setOpaque(false);
+        checkboxPanel.setBounds(0, 250, 850, 300);
+        layeredPane.add(checkboxPanel, JLayeredPane.PALETTE_LAYER);
+
+        // set up a button panel for the "repair" button
+        JPanel buttonPanel = getButtonJPanel(parent, checkboxPanel);
+        buttonPanel.setOpaque(false);
+        buttonPanel.setBounds(0, 320, 850, 100); // position at top
         layeredPane.add(buttonPanel, JLayeredPane.PALETTE_LAYER);
 
+        // set-up the base component for the decorator (gleebu' broken spaceship)
         gleebusShip.addComponent();
         System.out.println("Gleebus ship : " + gleebusShip.getComponents().getFirst());
 
-//        add(drawingPanel, BorderLayout.NORTH);
-//        //add(buttonPanel, BorderLayout.NORTH);
+        JPanel textPanel = getTextPanel(parent);
+        textPanel.setOpaque(false);
+        textPanel.setBounds(0, 50, 850,300);
+        layeredPane.add(textPanel, JLayeredPane.PALETTE_LAYER);
 
     }
 
-    private JPanel getButtonJPanel(JFrame frame) {
-        JButton myButton = new JButton("REPAIR");
+    private JPanel getButtonJPanel(JFrame frame, JPanel checkboxPanel) {
+        JButton repairButton = new JButton("REPAIR");
+        repairButton.setPreferredSize(new Dimension(100,50));
         JPanel buttonPanel = new JPanel();
         //buttonPanel.setLayout(null);
         //myButton.setBounds(590,400,100,25);
         buttonPanel.setLayout(new FlowLayout());
 
-        myButton.addActionListener(new ActionListener() {
+        repairButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Code to execute when the button is clicked
                 System.out.println("Button was clicked!");
-                Spaceship leverComponent = new LeverDecorator(gleebusShip);
-                leverComponent.addComponent();
-                leverComponent.setVisible(true);
-                System.out.println("Gleebus ship : " + gleebusShip.getComponents().get(1));
 
-                Spaceship buttonPanelComponent = new ButtonPanelDecorator(gleebusShip);
-                buttonPanelComponent.addComponent();
-                buttonPanelComponent.setVisible(true);
+                if ((hammer.isSelected() &&  wrench.isSelected() && !screwdriver.isSelected() && !pliers.isSelected())
+                        && !screenComponentBuilt){
+                    Spaceship screenComponent = new ScreenDecorator(gleebusShip);
+                    screenComponent.addComponent();
+                    screenComponent.setVisible(true);
+                    screenComponentBuilt = true;
+                } else if ((pliers.isSelected() && wrench.isSelected() && screwdriver.isSelected() && !hammer.isSelected())
+                        && !buttonPanelComponentBuilt){
+                    Spaceship buttonPanelComponent = new ButtonPanelDecorator(gleebusShip);
+                    buttonPanelComponent.addComponent();
+                    buttonPanelComponent.setVisible(true);
+                    buttonPanelComponentBuilt = true;
+                } else if ((pliers.isSelected() && screwdriver.isSelected() && !wrench.isSelected() && !hammer.isSelected())
+                        && !leverComponentBuilt){
+                    Spaceship leverComponent = new LeverDecorator(gleebusShip);
+                    leverComponent.addComponent();
+                    leverComponent.setVisible(true);
+                    leverComponentBuilt = true;
+                }
 
-                Spaceship screenComponent = new ScreenDecorator(gleebusShip);
-                screenComponent.addComponent();
-                screenComponent.setVisible(true);
                 parent.getContentPane().repaint();
             }
         });
 
-        buttonPanel.add(myButton);
+        buttonPanel.add(repairButton);
         return buttonPanel;
+    }
+
+    private JPanel getCheckBoxPanel(JFrame frame){
+        JPanel checkboxPanel = new JPanel();
+        checkboxPanel.setLayout(new FlowLayout());
+
+        Font checkboxFont = new Font("Arial", Font.PLAIN, 24);
+
+        hammer = new JCheckBox("Hammer");
+        hammer.setFont(checkboxFont);
+        wrench = new JCheckBox("Wrench");
+        wrench.setFont(checkboxFont);
+        screwdriver = new JCheckBox("Screwdriver");
+        screwdriver.setFont(checkboxFont);
+        pliers = new JCheckBox("Pliers");
+        pliers.setFont(checkboxFont);
+
+
+
+        checkboxPanel.add(hammer);
+        checkboxPanel.add(wrench);
+        checkboxPanel.add(screwdriver);
+        checkboxPanel.add(pliers);
+
+        return checkboxPanel;
+    }
+
+    private JPanel getTextPanel(JFrame frame){
+        JLabel title = new JLabel("REPAIR THE SPACESHIP CONTROL PANEL");
+        title.setFont(new Font("Serif", Font.PLAIN, 40));
+        title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+        JLabel descriptionLabel = new JLabel("Some of the control panel components broke in the crash!");
+        descriptionLabel.setFont(new Font("Dialog", Font.PLAIN, 24));
+        descriptionLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+        JLabel descriptionLabel2 = new JLabel("Help Gleebus each one by selecting the appropriate tools");
+        descriptionLabel2.setFont(new Font("Dialog", Font.PLAIN, 18));
+        descriptionLabel2.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+        JLabel descriptionLabel3 = new JLabel("and then hitting the repair button.");
+        descriptionLabel3.setFont(new Font("Dialog", Font.PLAIN, 18));
+        descriptionLabel3.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+        JPanel textPanel = new JPanel();
+        textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS)); // box layout, stack components vertically
+
+        textPanel.add(title);
+        textPanel.add(descriptionLabel);
+        textPanel.add(descriptionLabel2);
+        textPanel.add(descriptionLabel3);
+
+
+        return textPanel;
     }
 
     private class DrawPanel extends JPanel {
@@ -72,6 +155,10 @@ public class Level_3 extends JPanel {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
 
+            Image manual = new ImageIcon("src/Images/Puzzle3/P3_Manual.png").getImage();
+            g.drawImage(manual, 875,10, this);
+
+            // handle drawing spaceship
             if (gleebusShip.getComponents() != null){
                 for(Spaceship spaceship : gleebusShip.getComponents()) {
 
