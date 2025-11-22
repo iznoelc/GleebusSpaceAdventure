57da9ea00ccb82d386265b3160ce6d5cd5393fb9
iznoelc <izzybellanoel@gmail.com>
2025-11-21 21:28:29 -0500

    add comments to complete classes

--- src/Game/NextCard.java
+++ src/Game/NextCard.java
@@ -1,3 +1,7 @@
+/**
+ * Helper class to handle transition between cards.
+ * @author Izzy Carlson
+ */
 package Game;
 
 import javax.swing.*;
@@ -5,7 +9,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class NextCard {
+    /**
+     * Creates a button that, when clicked, moves to the next specified card.
+     * @param parent Game JFrame
+     * @param buttonText What the button should say (i.e. START, NEXT LEVEL)
+     * @param width Button's width
+     * @param height Button's height
+     * @param nextCard The name of the card that the button should move to when clicked
+     * @return a JPanel with the next card button
+     */
     public JPanel getNextCardButtonPanel(Game parent, String buttonText, int width, int height, String nextCard){
         JPanel buttonPanel = new JPanel();
         JButton startButton = new JButton(buttonText);
--- src/Game/Start.java
+++ src/Game/Start.java
@@ -1,3 +1,7 @@
+/**
+ * Handles set-up for the start screen.
+ * @author Izzy Carlson
+ */
 package Game;
 
 import javax.swing.*;
@@ -6,18 +10,25 @@ import java.awt.*;
 public class Start extends JPanel {
     JFrame parent;
 
+    /**
+     * Constructor to set up the start card.
+     * @param parent the Game JFrame
+     */
     public Start(Game parent){
         this.parent = parent;
         setLayout(new BorderLayout());
 
+        // sets up the layered pane to store logo and button on
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setPreferredSize(new Dimension(1280,720));
         add(layeredPane, BorderLayout.CENTER);
 
+        // drawing panel to draw the logo on, set as default layer of layered pane
         DrawPanel drawingPanel = new DrawPanel();
         drawingPanel.setBounds(0,0,1280,720);
         layeredPane.add(drawingPanel, JLayeredPane.DEFAULT_LAYER);
 
+        // set up the START button
         NextCard nextCard = new NextCard();
         JPanel buttonPanel = nextCard.getNextCardButtonPanel(parent, "START", 200, 75, "Level3");
         buttonPanel.setOpaque(false);
@@ -25,10 +36,12 @@ public class Start extends JPanel {
         layeredPane.add(buttonPanel, JLayeredPane.PALETTE_LAYER);
     }
 
+    /**
+     * Handles drawing images.
+     */
     private class DrawPanel extends JPanel {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-
             Image logo = new ImageIcon("src/Images/Logo.png").getImage();
             g.drawImage(logo,148, 100, this);
         }
